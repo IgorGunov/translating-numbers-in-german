18031df9b0af386c2df2ebf4dd32abecf123e937
Igor <igorgunov1564@yandex.ru>
2022-10-08 19:37:27 +0300

    работа в окне

--- src/Main.java
+++ src/Main.java
@@ -1,85 +1,76 @@
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите число словом");
-        int units = 0;
-        int unitsEnd = 0;
-        int dozens = 0;
-        int hundreds = 0;
-        int hundredsStart = 0;
-        int hundredsEnd = 0;
-        int sum = 0;
-        String line = scanner.nextLine();
-        String text = line.replaceAll("\\s","");
-        String text2;
+        while (true) {
+            int units = 0;
+            int unitsEnd = 0;
+            int dozens = 0;
+            int hundreds = 0;
+            int hundredsStart = 0;
+            int hundredsEnd = 0;
+            int sum = 0;
+            String line = JOptionPane.showInputDialog("Введите число словом");
+            String text = line.replaceAll("\\s", "");
+            String text2;
 
 
-        for (int i = 0; i < text.length() - 7; i ++) {
-            int x = i + 7;
+            for (int i = 0; i < text.length() - 7; i++) {
+                int x = i + 7;
 
-            if (text.substring(i,x).equals("hundert")) {
-                hundredsStart = i;
-                hundredsEnd = x;
-                if (hunders(text.substring(0, hundredsStart)) != 10) {
-                    hundreds = hunders(text.substring(0, hundredsStart));
+                if (text.substring(i, x).equals("hundert")) {
+                    hundredsStart = i;
+                    hundredsEnd = x;
+                    if (hunders(text.substring(0, hundredsStart)) != 10) {
+                        hundreds = hunders(text.substring(0, hundredsStart));
+                    }
+                }
+                if (text.substring(i + 1).equals("hundert")) {
+                    hundredsStart = i + 1;
+                    hundredsEnd = x;
+                    if (hunders(text.substring(0, hundredsStart)) != 10) {
+                        hundreds = hunders(text.substring(0, hundredsStart));
+                    }
                 }
             }
-            if (text.substring(i + 1).equals("hundert")) {
-                hundredsStart = i + 1;
-                hundredsEnd = x;
-                if (hunders(text.substring(0, hundredsStart)) != 10) {
-                    hundreds = hunders(text.substring(0, hundredsStart));
+
+
+            for (int i = hundredsEnd; i < text.length(); i++) {
+
+                if (units(text.substring(hundredsEnd, i)) != 10) {
+                    units = units(text.substring(hundredsEnd, i));
+                    unitsEnd = i;
+                }
+
+                if (units(text.substring(hundredsEnd)) != 10) {
+                    units = units(text.substring(hundredsEnd));
+                }
+
+                if (i == text.length()) {
+                    units = units(text.substring(hundredsEnd, i + 1));
                 }
             }
-        }
 
-
-        for (int i = hundredsEnd; i < text.length(); i ++) {
-
-            if (units(text.substring(hundredsEnd, i)) != 10) {
-                units = units(text.substring(hundredsEnd, i));
-                unitsEnd = i;
+            for (int i = unitsEnd; i < text.length(); i++) {
+                text2 = text.substring(i);
+                if (dozens(text2) != 10) {
+                    dozens = dozens(text2);
+                }
             }
 
-            if (units(text.substring(hundredsEnd)) != 10) {
-                units = units(text.substring(hundredsEnd));
+            if (hundreds != 0) {
+                sum = sum + hundreds * 100;
             }
 
-            if (i == text.length()) {
-                units = units(text.substring(hundredsEnd, i + 1));
+            if (dozens != 0) {
+                sum = sum + dozens * 10;
             }
-        }
 
-        for (int i = unitsEnd; i < text.length(); i ++) {
-            text2 = text.substring(i);
-            if (dozens(text2) != 10) {
-                dozens = dozens(text2);
+            if (units != 0) {
+                sum = sum + units;
             }
+            JOptionPane.showMessageDialog(null, sum);
         }
-
-        if (hundreds != 0) {
-            sum = sum + hundreds * 100;
-        } else {
-            System.out.println("Ошибка в сотнях");
-            return;
-        }
-
-        if (dozens != 0) {
-            sum = sum + dozens * 10;;
-        } else {
-            System.out.println("Ошибка в десятках");
-            return;
-        }
-
-        if (units != 0) {
-            sum = sum + units;;
-        } else {
-            System.out.println("Ошибка в единицах");
-            return;
-        }
-        System.out.print(sum);
     }
 
     public static int units(String text) {
@@ -158,4 +149,5 @@ public class Main {
         }
         return 10;
     }
+
 }
